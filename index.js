bb58e6eed72d364a9ab74f22c48201351eb1ea1d
Jaehyuk-Lee <alex00728@outlook.com>
2024-12-12 13:18:31 +0900

    connection 파트 try catch finally 모듈화

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import config from "./config/config.js";
 import { objectToArray } from "./util/util.js";
 import sggcd from "./sggcd/sggcd.js";
-import oracleRegioncd from "./sggcd/oracleRegioncd.js";
+import oracleUtil from './util/oracle.util.js';
 
 // 오라클 DB에 API로 가져온 법정동코드, 행정구역 한국어 이름 데이터 삽입
 let gu = sggcd.getRegionCdFromJson();
@@ -10,7 +10,8 @@ console.log(gu);
 const arr = objectToArray(gu, Object.keys(config.mapping.regionCd));
 console.log(arr);
 
-oracleRegioncd.runApp('REGIONCD', Object.entries(config.mapping.regionCd).map((row) => row[1]), arr);
+await oracleUtil.createRegionCdTable();
+oracleUtil.insertMany('REGIONCD', Object.entries(config.mapping.regionCd).map((row) => row[1]), arr);
 
 
 // 단독/다가구
